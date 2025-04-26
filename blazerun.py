0b6628f2de3ca08bf83f4f702628656437c2e811
A-Diary-Rat <adit.ajad@gmail.com>
2025-04-26 10:46:44 +0530

    Added files

---
+++ blazerun.py
@@ -0,0 +1,16 @@
+import cv2 
+
+def FrameCapture(path): 
+
+	vidObj = cv2.VideoCapture(path) 
+	count = 0
+	success = 1
+
+	while success: 
+		success, image = vidObj.read() 
+		cv2.imwrite("frame%d.jpg" % count, image) 
+		count += 1
+
+if __name__ == '__main__': 
+
+	FrameCapture("blaze/20240211_092524.mp4") 
